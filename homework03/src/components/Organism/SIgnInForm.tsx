92960bbe674f680df62a05c48840cd04a40c68a8
myeong-jae-hwi <audwognl@gmail.com>
2025-01-28 19:10:12 +0900

    [구성]: 유기체 (로그인 페이지) 구성

--- homework03/src/components/Organism/SIgnInForm.tsx
+++ homework03/src/components/Organism/SIgnInForm.tsx
@@ -1,10 +1,15 @@
 import InputComponent from '@/components/Molecule/InputForm';
+import './SignInForm.css';
 
 function SignInForm() {
   return (
-    <>
+    <div className="sign-in-form">
+      <h1 className="logo">
+        <img src="/src/assets/Logo.svg" alt="Coupang" />
+      </h1>
+      <h2 className="sr-only">로그인</h2>
       <InputComponent></InputComponent>
-    </>
+    </div>
   );
 }
 

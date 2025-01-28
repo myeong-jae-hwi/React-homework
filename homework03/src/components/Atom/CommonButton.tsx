44521109eea4df944c7e9d629c53631e1a0344ec
myeong-jae-hwi <audwognl@gmail.com>
2025-01-28 19:02:44 +0900

    [구성]: 원자 (버튼, 인풋) 컴포넌트 생성

--- homework03/src/components/Atom/CommonButton.tsx
+++ homework03/src/components/Atom/CommonButton.tsx
@@ -4,11 +4,33 @@ interface CommonButtonProps {
   children: string;
   type: 'submit' | 'button' | 'reset' | undefined;
   func?: () => void;
+  mode: 'primary' | 'secondary';
+  disabled: boolean;
 }
 
-function CommonButton({ children, type, func }: CommonButtonProps) {
-  return (
-    <button type={type} onClick={func}>
+function CommonButton({
+  children,
+  type,
+  func,
+  disabled,
+  mode,
+}: CommonButtonProps) {
+  return mode === 'primary' ? (
+    <button
+      type={type}
+      onClick={func}
+      className="common__button"
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  ) : (
+    <button
+      type={type}
+      onClick={func}
+      className="common__button__secondary"
+      disabled={disabled}
+    >
       {children}
     </button>
   );
--- homework03/src/components/Atom/InputComponent.tsx
+++ homework03/src/components/Atom/InputComponent.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { ReactEventHandler, useId } from 'react';
 import './InputCSS.css';
 
 interface InputComponentProps {
@@ -6,6 +6,8 @@ interface InputComponentProps {
   placeholder: string;
   type: string;
   icon: string;
+  error?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 function InputComponent({
@@ -13,17 +15,51 @@ function InputComponent({
   placeholder,
   type,
   icon,
+  error = false,
+  onChange,
 }: InputComponentProps) {
   const id = useId();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget);
+    const input = e.currentTarget.previousElementSibling as HTMLInputElement;
+    console.log('비밀번호 보이기 클릭');
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
+  };
+
   return (
-    <div className="input">
-      <div className="icon">
-        <img src={`src/assets/${icon}`} alt="" />
+    <div>
+      <div className={`input__container ${error ? 'error' : ''}`}>
+        <div className="icon">
+          <img src={`src/assets/${icon}`} alt="" />
+        </div>
+        <label htmlFor={id} className="sr-only">
+          {title}
+        </label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          id={id}
+          className="input"
+          onChange={onChange}
+        />
+        {type === 'password' ? (
+          <button type="button" className="visible" onClick={handleClick}>
+            <img src="/src/assets/visible.svg" alt="비밀번호 보이기" />
+          </button>
+        ) : null}
       </div>
-      <label htmlFor={id} className="sr-only">
-        {title}
-      </label>
-      <input type={type} placeholder={placeholder} id={id} />
+      {error ? (
+        <p className="error-msg">
+          {type === 'email'
+            ? '아이디(이메일)는 이메일 형식으로 입력해주세요.'
+            : '숫자, 영어 조합 6자리 이상 입력해야 합니다.'}
+        </p>
+      ) : null}
     </div>
   );
 }
